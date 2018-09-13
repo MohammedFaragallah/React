c80dc234b7ed009150848accb8ac274e74ff0d7b
Mohammed Faragallah <o0frego0o@hotmail.com>
2018-09-13 19:31:31 +0200

    added better expense items view

--- src/Components/Expenses/ExpensesListItem.js
+++ src/Components/Expenses/ExpensesListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpensesListItem = ({ id, description, amount, createdAt }) => (
   <div className="item">
@@ -7,7 +9,8 @@ export const ExpensesListItem = ({ id, description, amount, createdAt }) => (
       <p>{description}</p>
     </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format('$0,0.00')} -{' '}
+      {moment(createdAt).format('MMMM Do, YYYY')}
     </p>
   </div>
 );
